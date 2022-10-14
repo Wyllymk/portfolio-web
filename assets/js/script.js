32414d0917d115a9e2a38ae7ea7746dae4d28456
Wilson Mbuthia <wyllyjeremy@gmail.com>
2022-10-14 14:42:46 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
     // Typing animation
     var typed = new Typed(".typing", {
-        strings: ["Frontend Developer (Angular, React)", "Backend Developer (Django, Flask)", "Designer (Figma, Canva, Miro, Adobe Illustrator)"], // , "Freelancer", "Blogger"
+        strings: ["Frontend Developer (React)", "Backend Developer (PHP, Laravel)", "Designer (Figma, Canva, Adobe Illustrator)"], // , "Freelancer", "Blogger"
         typeSpeed: 70,
         backSpeed: 50,
         loop: true
